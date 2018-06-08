5d35b3a2f793669219bdf22c1543e940ce24c109
anisoncharts <llamaop2013b@gmail.com>
2018-06-08 21:38:10 +1000

    Update previewbot.js

--- previewbot.js
+++ previewbot.js
@@ -8,9 +8,9 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.channel.id === "298059784987607040") {
         if (message.content.includes("https://") || message.content.includes("http://")) {
-            client.channels.get("454601350072762378").send(message.content)
+            client.channels.get("454601350072762378").send(message.content);
         else {
-            client.channels.get("401395421769105431").send(message.content)
+            client.channels.get("401395421769105431").send(message.content);
         }
     }
 });
