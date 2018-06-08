8dd9706c0216d6a4bd71d845a15e9fd01e4c2204
anisoncharts <llamaop2013b@gmail.com>
2018-06-08 22:01:10 +1000

    Update previewbot.js

--- previewbot.js
+++ previewbot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.channel.id === "298059784987607040" && message.content.includes("https://")) {
-        client.channels.get("401395421769105431").send(message.content);
+    if (message.channel.id === "298059784987607040" && message.content.includes("http")) {
+        client.channels.get("454601350072762378").send(message.content);
     }
 });
 
