c79165f37ae506e58958e019d6035c1ae944cbbb
anisoncharts <llamaop2013b@gmail.com>
2018-06-13 13:49:32 +1000

    Update previewbot.js

--- previewbot.js
+++ previewbot.js
@@ -9,6 +9,10 @@ client.on('message', message => {
     if (message.channel.id === "298059784987607040" && message.content.includes("http")) {
         client.channels.get("454601350072762378").send(message.content);
     }
+    
+    if (message.channel.id === "311143593018392578" && message.content.includes("http") && message.author.id != "203303038243438592") {
+        client.channels.get("456302366556291072").send(message.content);
+    }
 });
 
 client.login(process.env.BOT_TOKEN);
