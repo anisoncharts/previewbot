5613645799d8d7ab3d6b4339fc7e3bbe79f3db1b
anisoncharts <llamaop2013b@gmail.com>
2022-09-06 17:10:00 +1000

    Update previewbot.js

--- previewbot.js
+++ previewbot.js
@@ -6,19 +6,17 @@ const client = new Client({
   ]
 });
 
-client.on('ready', () => {
+client.once('ready', () => {
     console.log('I am ready!');
-    var channel = client.channels.get("454601350072762378");
-    console.log(channel);
 });
 
-client.on('message', message => {
+client.on('messageCreate', message => {
     if (message.channel.id === "298059784987607040" && message.content.includes("http")) {
         client.channels.get("454601350072762378").send(message.content);
     }
-    
-    if (message.channel.id === "311143593018392578" && message.content.includes("http") && message.author.id != "203303038243438592") {
-        client.channels.get("456302366556291072").send(message.content);
+  
+    if (message.channel.id === "1016602103604584528" && message.content.includes("http")) {
+        client.channels.get("454601350072762378").send(message.content);
     }
 });
 
